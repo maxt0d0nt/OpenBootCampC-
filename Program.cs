688c5c607b350379e63c056b4f6673f99ba028b7
maxt0d0nt <maxbox21@hotmail.com>
2023-02-08 16:00:12 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// ejercicio 1
+Console.WriteLine("¿Como te llamas?");
+var name = Console.ReadLine();
+Console.WriteLine("Tu nombre es " + name );
+
+
+//ejercicio2
+Console.WriteLine("La fecha y hora actual es:");
 Console.WriteLine(DateTime.Now);
