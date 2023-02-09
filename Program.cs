526c2b0c13d2fda8c0d898a0eb212a1fb60359b9
maxt0d0nt <maxbox21@hotmail.com>
2023-02-09 23:26:22 +0100

    tema 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿// ejercicio 1
 Console.WriteLine("¿Como te llamas?");
 var name = Console.ReadLine();
-Console.WriteLine("Tu nombre es " + name );
+Console.WriteLine("Tu nombre es " + name);
+
 
 
 //ejercicio2
 Console.WriteLine("La fecha y hora actual es:");
 Console.WriteLine(DateTime.Now);
+
+//TEMA 2
+//Ejercicio 1 nombre, apellido, edad, sabe programar, etc.
+Console.WriteLine("Escriba su nombre:");
+var name = Console.ReadLine();
+Console.WriteLine("Escriba su apellido:");
+var lastName = Console.ReadLine();
+Console.WriteLine("Escriba su edad:");
+var age = Console.ReadLine();
+Console.WriteLine("Sabe programar:");
+var knowDevelope = Console.ReadLine();
+
+Console.WriteLine("Su nombre completo es "+ name + " "+ lastName);
+Console.WriteLine("Su edad es " + age);
+Console.WriteLine("¿Sabe Programar? " + knowDevelope);
+
